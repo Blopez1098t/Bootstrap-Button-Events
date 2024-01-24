3bb1d0ab32c1a475dec199d56c5ffe3a037bb129
Bryan <125blopez@frhsd.com>
2024-01-24 08:32:24 -0500

    please help

--- Bootstrap Button Events/js/clickEvent.js
+++ Bootstrap Button Events/js/clickEvent.js
@@ -1,6 +1,18 @@
 let btnOne = document.getElementById('click')
-btnOne.addEventListener("click", makeGreen)
+btnOne.addEventListener("click", function(event){
+    event.target.style.background = "#a0b96b"
+});
 
-function makeGreen(){
-    btnOne.style.background = "#a0b96b";
-}
+let btnTwo = document.getElementById('dblclick');
+btnTwo.addEventListener('dblclick', function(e){
+    e.target.style.background = "red";
+})
+
+let btnThree = document.getElementById('onmouseover');
+btnThree.addEventListener("mouseover", function(ev){
+    ev.target.style.background = "orange";
+})
+
+let btnFour = document.getElementById('onmouseup');
+btnFour.addEventListener("mouseup", function(eve){
+    eve.target.style.background = "blue";
