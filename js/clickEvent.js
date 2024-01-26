0b5d3ceceec2dfb115b9e14248b833c29c92846c
Bryan <125blopez@frhsd.com>
2024-01-26 10:18:57 -0500

    hhehu

--- js/clickEvent.js
+++ js/clickEvent.js
@@ -17,3 +17,33 @@ let btnFour = document.getElementById('onmouseup');
 btnFour.addEventListener("mouseup", function(eve){
     eve.target.style.background = "blue";
 })
+
+let tx1 = document.getElementById('text1');
+tx1.addEventListener("keydown", function(even){
+    even.target.style.background = "red";
+});
+
+let tx2 = document.getElementById('text2');
+tx2.addEventListener("keyup", function(eventt){
+    eventt.target.style.background = "green";
+});
+
+let tx3 = document.getElementById('text3');
+tx3.addEventListener("keypress", function(eventtt){
+    eventtt.target.style.background = "purple";
+});
+
+const tx4s = [
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
+  ];
+  
+  const tx4t = {
+    duration: 500,
+    iterations: 1,
+  };
+  
+  const tx4 = document.getElementById("text4");
+  
+  tx4.addEventListener("keypress", () => {
+    tx4.animate(tx4s, tx4t);})
